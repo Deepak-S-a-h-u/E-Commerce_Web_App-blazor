cef428e5147520611efb2543571ed06e0048112e
“[Deepak <[deepak-sahu@cssoftsolutions.com]>
2023-09-29 15:53:30 +0530

    Added Api Project

---
+++ ScheduleTransportationInstructions.API/Controllers/CarController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarRepository _repository;
+        public CarController(ICarRepository repository)
+        {
+                _repository = repository;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Domain.Entities.Car>> GetAllCars () 
+        {
+             var cars=await _repository.GetCars();
+            return cars;
+        }
+        [HttpPost]
+        public async Task<Car> AddCar(Car carDto )
+        {
+            var cars = await _repository.AddCar(carDto);
+            return cars;
+        }
+    }
+}
